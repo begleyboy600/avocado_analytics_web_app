02e4b7b961b687591a2bfb4df909281b51e3dc8a
conor <conorbegley5@gmail.com>
2023-01-08 23:48:40 +0000

    final commit. NOTE: This data visualization app is only a base for my actual project

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import numpy as np
+from dash import Output, Input
 
 data = pd.read_csv("avocado.csv")
 data = data.query("type == 'conventional' and region == 'Albany'")
@@ -123,7 +124,7 @@ app.layout = html.Div(
         html.Div(
             children=
             dcc.Graph(
-                id="price-chart",
+                id="volume-chart",
                 config={"displayModeBar": False},
                 figure={
                     "data": [
@@ -152,37 +153,79 @@ app.layout = html.Div(
             ),
             className="card",
         ),
+
     ],
 )
 
-# html.Div(
-#     children=[
-#         dcc.Graph(
-#             figure={
-#                 "data": [
-#                     {
-#                         "x": data["Date"],
-#                         "y": data["AveragePrice"],
-#                         "type": "lines"
-#                     },
-#                 ],
-#                 "layout": {"title": "Average Price of Avocados"},
-#             },
-#         ),
-#         dcc.Graph(
-#             figure={
-#                 "data": [
-#                     {
-#                         "x": data["Date"],
-#                         "y": data["Total Volume"],
-#                         "type": "lines"
-#                     },
-#                 ],
-#                 "layout": {"title": "Avocados Sold"}
-#             },
-#         ),
-#     ]
-# )
+@app.callback(
+    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    [
+        Input("region-filter", "value"),
+        Input("type-filter", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+def update_charts(region, avocado_type, start_date, end_date):
+    mask = (
+        (data.region == region)
+        & (data.type == avocado_type)
+        & (data.Date >= start_date)
+        & (data.Date <= end_date)
+    )
+    filtered_data = data.loc[mask, :]
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["AveragePrice"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average Price of Avocados",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Total Volume"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Avocados Sold",
+                "x": 0.05,
+                "xanchor": "left"
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
 
 if __name__ == "__main__":
     app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
+
+
